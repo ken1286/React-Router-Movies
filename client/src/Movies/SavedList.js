9e8c1d7f116734235fe669cee06a870b1f5b0922
ken1286 <kindle1286@gmail.com>
2019-05-06 15:24:38 -0700

    MVP with stretch saved list

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Movie from './Movie';
 
 export default class SavedList extends Component {
   constructor(props) {
     super(props);
   }
 
+  // addToSavedList = () => {
+  //   this.setState({
+  //     savedList: [...this.state.savedList, props.movie]
+  //   })
+  //   console.log(this.state.savedList);
+  // }
+
   render() {
     return (
       <div className="saved-list">
         <h3>Saved Movies:</h3>
-        {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
-        ))}
-        <div className="home-button">Home</div>
+        {this.props.list.map(movie => {
+          return (
+          <Link to={`/movies/${movie.id}`} key={movie.id}>
+            <span className="saved-movie">{movie.title}</span>
+          </Link>)
+        }
+        )}
+        <Link to="/"><div className="home-button">Home</div></Link>
       </div>
     );
   }
